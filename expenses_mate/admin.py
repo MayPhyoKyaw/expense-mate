98d122d583b549a703e7e629463a036256a8beb5
MayPhyoKyaw <mayphyokyaw.ff@gmail.com>
2024-10-22 21:34:05 +0900

    add model to admin

--- expenses_mate/admin.py
+++ expenses_mate/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import User, ExpenseGroup, Expense, GroupUser, Balance
+
 
 # Register your models here.
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'name', 'email', 'password_hash')
+
+
+class ExpenseGroupsAdmin(admin.ModelAdmin):
+    list_display = ('group_id', 'title', 'created_by', 'created_at')
+
+
+class GroupUsersAdmin(admin.ModelAdmin):
+    list_display = ('group_user_id', 'group_id', 'user_id')
+
+
+class ExpensesAdmin(admin.ModelAdmin):
+    list_display = ('expense_id', 'group_id', 'user_id', 'item_name', 'amount', 'transaction_type')
+
+
+class BalancesAdmin(admin.ModelAdmin):
+    list_display = ('balance_id', 'group_id', 'user_id', 'balance_amount', 'last_updated')
+
+
+admin.site.register(User, UsersAdmin)
+admin.site.register(ExpenseGroup, ExpenseGroupsAdmin)
+admin.site.register(GroupUser, GroupUsersAdmin)
+admin.site.register(Expense, ExpensesAdmin)
+admin.site.register(Balance, BalancesAdmin)
--- expenses_mate/models.py
+++ expenses_mate/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
-class Users(models.Model):
+class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
@@ -12,29 +11,29 @@ class Users(models.Model):
         return self.name
 
 
-class ExpenseGroups(models.Model):
+class ExpenseGroup(models.Model):
     group_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 
-class GroupUsers(models.Model):
+class GroupUser(models.Model):
     group_user_id = models.AutoField(primary_key=True)
-    group_id = models.ForeignKey(ExpenseGroups, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(ExpenseGroup, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user_id} in {self.group_id}'
 
 
-class Expenses(models.Model):
+class Expense(models.Model):
     expense_id = models.AutoField(primary_key=True)
-    group_id = models.ForeignKey(ExpenseGroups, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(ExpenseGroup, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=10, choices=[('add', 'Add'), ('subtract', 'Subtract'),
@@ -45,10 +44,10 @@ class Expenses(models.Model):
         return self.item_name
 
 
-class Balances(models.Model):
+class Balance(models.Model):
     balance_id = models.AutoField(primary_key=True)
-    group_id = models.ForeignKey(ExpenseGroups, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(ExpenseGroup, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     balance_amount = models.DecimalField(max_digits=10, decimal_places=2)
     last_updated = models.DateTimeField(auto_now=True)
 
