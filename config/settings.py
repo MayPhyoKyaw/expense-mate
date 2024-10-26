b87c13ef5b9e936da1632fcf3bd1ff7cee55b2e8
MayPhyoKyaw <mayphyokyaw.ff@gmail.com>
2024-10-27 01:26:07 +0900

    Add list page with customized style

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,15 +38,15 @@ ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
--- expenses_mate/views.py
+++ expenses_mate/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import User, ExpenseGroup, GroupUser, Expense, Balance
+
 
 # Create your views here.
+class ExpenseListView(ListView):
+    model = Expense
+    template_name = 'expenses_mate/expense_list.html'
+
+    # def get(self, request, *args, **kwargs):
+    #     context = self.get_context_data(**kwargs)
+    #     print("IndexViewを使ってTOP画面を表示します！")
+    #     return self.render_to_response(context)
