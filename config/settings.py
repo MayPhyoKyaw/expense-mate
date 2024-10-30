c9552635be80fdafceaab4581a6d3548bcd06e2e
MayPhyoKyaw <mayphyokyaw.ff@gmail.com>
2024-10-30 23:21:24 +0900

    add logout

--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,11 @@ SITE_ID =1
 # LOGIN_REDIRECT_URL = reverse_lazy("expenses_mate:expense_group_list")
 LOGIN_REDIRECT_URL = 'expenses_mate:expense_group_list'
 # LOGIN_REDIRECT_URL = 'expenses_mate:expenses_list'
+LOGOUT_REDIRECT_URL = 'account_login'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 # LOGIN_URL = 'account_login'
+ACCOUNT_LOGOUT_ON_GET = True  # This ensures that logout works with GET requests
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
